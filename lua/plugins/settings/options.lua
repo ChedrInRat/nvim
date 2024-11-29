903e2e1e193bb8fac3bc87ef7f6b74dbc4f3656a
ChedrInRat <myshchenko.maxym@gmail.com>
2024-11-29 00:31:24 +0000

    add transparency

--- lua/plugins/settings/options.lua
+++ lua/plugins/settings/options.lua
@@ -1,1 +1,5 @@
-vim.cmd.colorscheme("kanagawa-dragon")
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NonText guibg=NONE ctermbg=NONE
+]]
+-- vim.cmd.colorscheme("kanagawa-dragon")
