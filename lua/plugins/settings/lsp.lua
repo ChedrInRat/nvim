235eaa141a62cb5a97055a239cba9b8a192a7f75
ChedrInRat <myshchenko.maxym@gmail.com>
2025-07-13 15:28:04 +0000

    update mason

--- lua/plugins/settings/lsp.lua
+++ lua/plugins/settings/lsp.lua
@@ -5,32 +5,31 @@ local cpb = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 m.setup({
-    -- ensure_installed = {
-    --   'lua_ls',
-    --   'rust_analyzer',
-    --   'pyright',
-    --   'tsserver',
-    --   'sqls',
-    --   'marksman',
-    --   'gopls',
-    --   'clangd',
-    --   'cssls'
-    --   },
+    ensure_installed = {
+      'lua_ls',
+      'rust_analyzer',
+      'pyright',
+      'sqls',
+      'marksman',
+      'gopls',
+      'clangd',
+      'cssls'
+      },
 
 })
 
 
-m.setup_handlers {
-  function (server_name)
-    lspconfig[server_name].setup({
-      capabilities = cpb,
-      settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-    })
-  end
-}
+-- m.setup_handlers {
+--   function (server_name)
+--     lspconfig[server_name].setup({
+--       capabilities = cpb,
+--       settings = {
+--         Lua = {
+--             diagnostics = {
+--                 globals = { 'vim' }
+--             }
+--         }
+--     }
+--     })
+--   end
+-- }
